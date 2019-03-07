2f76359402e73b305340b5ede3b95bc1378457b4
Shtille <v.shtille@gmail.com>
2019-03-08 02:16:09 +0300

    Update sync_demos_resources.sh

--- sync_demos_resources.sh
+++ sync_demos_resources.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Script for resource syncronization between 'scythe/data' and 'demos/data'.
+# 'demos/data' fully includes 'scythe/data' and extends it.
+# Should be called from this directory.
 cd scythe/data
 rsync -r -R * ../../demos/data
